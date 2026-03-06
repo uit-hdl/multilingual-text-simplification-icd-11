264cb66e39680b80722f571c6656388c7277353f
Olga <olga.pelloni@gmail.com>
2026-03-06 20:18:25 +0100

    Added initial code for extracting ICD texts

---
+++ src/extract_dutch_icd_texts.py
@@ -0,0 +1,149 @@
+"""
+Extract ICD-10 texts in Dutch
+available at https://class.whofic.nl/
+"""
+
+from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ElementTree
+import zipfile
+import logging
+import requests
+import os
+import re
+
+
+# find a link to the latest release of ICD-10
+def find_icd_10_link():
+    url = "https://www.whofic.nl/downloads-en-links/icd-10-bestanden-voor-gebruik-in-lmrlbz"
+    r = requests.get(url)
+
+    # parse HTML
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    # get table with links to files
+    table = soup.find("table")
+
+    # latest version of ICD-10 is in the first row (after the header)
+    latest_version = table.find_all("tr")[1]
+
+    # find the latest link to the document page
+    cols = latest_version.find_all("td")
+    link = cols[0].find("a")["href"]
+
+    # get the link to *.zip file from that page
+    r = requests.get(link)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    zip_link = None
+
+    for link in soup.find_all("a", href=True):
+        if link["href"].endswith(".zip"):
+            zip_link = link["href"]
+
+    return zip_link
+
+
+# download the latest release of ICD-10 in *.xml format
+def download_icd_10_file(url):
+    filepath = '../data/dutch/dutch_icd10'
+
+    # save file
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(f"{filepath}.zip", "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+    # unzip
+    with zipfile.ZipFile(f"{filepath}.zip", "r") as z:
+        z.extractall(filepath)
+
+    # remove also the zip file
+    os.remove(f"{filepath}.zip")
+
+
+# parse XML tree and find the definition for the given ICD code
+def parse_xml_icd10(filepath, icd_code):
+    result = ''
+
+    tree = ElementTree.parse(filepath)
+    root = tree.getroot()
+
+    # find all <Class> elements recursively
+    for class_tag in root.findall(".//Class"):
+        code = class_tag.get("code")
+        found_class = class_tag
+
+        # get the definition when found the needed ICD code
+        if code == icd_code:
+            for rubric in found_class.findall(".//Rubric"):
+                kind = rubric.get("kind")
+                if kind == "description":
+                    text = ElementTree.tostring(rubric, encoding="unicode")
+
+                    # remove all tags and extra spaces
+                    without_tags = re.sub(r"<[^>]+>", "", text)
+                    clean_text = "\n".join(line.strip() for line in without_tags.splitlines() if line.strip())
+                    result += clean_text + ' '
+
+    return result
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+
+    dutch_folder = "../data/dutch/dutch_icd10"
+
+    # F20: code for Schizophrenia in ICD-10 we don't get sub codes,
+    # because they significantly differ in content from ICD-11 (different types of schizophrenia described)
+    # description of F20 in ICD-10 is relatively similar to 6A20 in ICD-11
+    icd_code = "F20"
+
+    # check if .xml file already exists
+    xml_exists = any(
+        f.lower().endswith(".xml") and os.path.isfile(os.path.join(dutch_folder, f))
+        for f in os.listdir(dutch_folder)
+    )
+
+    if not xml_exists:
+        logger.info(f"Finding a link to the latest Dutch ICD-10")
+        icd10_url = find_icd_10_link()
+
+        # just to write it down once and use it later when parsing XML and writing results into the TSV file
+        with open(f'{dutch_folder}/release_url_link.txt', 'w') as f:
+            f.write(icd10_url)
+
+        logger.info(f"Found a link: {icd10_url}")
+        logger.info(f"Downloading the ZIP file")
+        download_icd_10_file(icd10_url)
+
+        logger.info(f"XML ICD-10 file is ready")
+
+    # find XML file
+    dutch_icd10_filepath = ''
+    for filename in os.listdir(dutch_folder):
+        if filename.endswith('xml'):
+            dutch_icd10_filepath = os.path.join(dutch_folder, filename)
+            break
+
+    logger.info(f"Extracting description for the code {icd_code}")
+    result_text = parse_xml_icd10(dutch_icd10_filepath, icd_code)
+
+    # we assume that ICD-11 has been extracted first and the file exists
+    # replace F (ICD-10) to 6A (ICD-11)
+    icd_code = icd_code.replace('F', '6A')
+    results_path = f"../results/parallel_icd_texts_{icd_code}.tsv"
+
+    # get link for the latest release
+    with open(f'{dutch_folder}/release_url_link.txt', 'r') as f:
+        icd10_url = f.read()
+
+    # nl code stands for dutch
+    logger.info(f"Writing extracted text to results file: {results_path}")
+    with open(results_path, 'a') as f:
+        f.write(f"{icd10_url}\tnl\t{icd_code}\t{result_text}\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/extract_icd_texts.py
@@ -0,0 +1,143 @@
+"""
+A pipeline extracting texts in different languages from ICD-11
+and making a parallel TSV corpus
+"""
+
+import logging
+import requests
+import pandas as pd
+import os
+
+TOKEN_URL = "https://icdaccessmanagement.who.int/connect/token"
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+
+# get texts in for a specific disease code
+def find_texts_by_icd_code(headers, latest_release_url, icd_code):
+    # list of tuples (code, text)
+    results = []
+
+    url = f"{latest_release_url}"
+
+    # also show text with diagnostic criteria
+    params = {
+        "include": "diagnosticCriteria"
+    }
+
+    # browse mental disorders
+    r = requests.get(url, headers=headers).json()
+    for child in r['child']:
+        disease_class = requests.get(child, headers=headers).json()
+
+        # code 06 for "Mental, behavioural or neurodevelopmental disorders"
+        if disease_class['code'] == '06':
+
+            # browse subclass; it doesn't have a code, so we continue browsing the children links of all subclasses
+            for disease_class_child in disease_class['child']:
+                disease_subclass = requests.get(disease_class_child, headers=headers).json()
+
+                # browse diseases
+                if disease_subclass.get('child'):
+                    for disease_subclass_child in disease_subclass['child']:
+                        disease = requests.get(disease_subclass_child, headers=headers, params=params).json()
+
+                        # locate schizophrenia by its code
+                        if disease['code'] == icd_code:
+
+                            # add to results
+                            if disease.get('definition'):
+                                results.append((icd_code, disease['definition']['@value']))
+
+                            # if there are diagnostic criteria,
+                            # also add a row with the same code + '_criteria'
+                            # if disease.get('diagnosticCriteria'):
+                            #     results.append((icd_code + '_criteria', disease['diagnosticCriteria']['@value']))
+
+                            # check if there are additional subpages to this definition
+                            if disease.get('child'):
+                                for found_disease_child in disease['child']:
+                                    sub_disease_page = requests.get(found_disease_child, headers=headers).json()
+
+                                    # add to results the definitions of subpages
+                                    if sub_disease_page.get('definition'):
+                                        results.append((sub_disease_page['code'],
+                                                        sub_disease_page['definition']['@value']))
+
+    return results
+
+
+# access ICD API and get a definition in a chosen language
+# available TRUSTING languages: en, fr, cs, tr, de
+# to access ICD API: register at https://icd.who.int/icdapi
+def get_icd_text(lang="en", icd_code="6A20"):
+    token_resp = requests.post(
+        TOKEN_URL,
+        data={
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "scope": "icdapi_access",
+            "grant_type": "client_credentials",
+        },
+    )
+
+    token = token_resp.json()["access_token"]
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Accept": "application/json",
+        "Accept-Language": lang,
+        "API-Version": 'v2'
+    }
+
+    icd_entity_url = "https://id.who.int/icd/entity"
+
+    # open the initial entity page first
+    r = requests.get(icd_entity_url, headers=headers)
+    icd_entity_data = r.json()
+
+    # find the latest release of the ICD definitions
+    latest_release_url = icd_entity_data['includedLinearizations'][0]
+
+    # browse ICD and find the definition by the disease's code
+    results = find_texts_by_icd_code(headers, latest_release_url, icd_code)
+    return latest_release_url, results
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+
+    # list of needed and available languages:
+    # English, French, Czech, Turkish, German
+    languages = ['en', 'fr', 'cs', 'tr', 'de']
+
+    # 6A20: code for Schizophrenia
+    icd_code = "6A20"
+
+    # rows to write to a dataframe
+    rows = []
+
+    # open a csv file
+    results_path = f"results/parallel_icd_texts_{icd_code}.tsv"
+    if not os.path.exists(results_path):
+        f = open(results_path, "w")
+        f.write('release_url\tlanguage\tcode\toriginal_text\n')
+
+    for language in languages:
+        logger.info(f"Loading texts in language: {language}")
+        release_url, results = get_icd_text(language, icd_code)
+
+        for result in results:
+            logger.info("Writing a row of results")
+            rows.append({"release_url": release_url,
+                         "language": language,
+                         "code": result[0],
+                         "original_text": result[1]})
+
+    df = pd.DataFrame(rows)
+    df.to_csv(results_path, mode='a', sep='\t', index=False)
+
+
+if __name__ == '__main__':
+    main()
